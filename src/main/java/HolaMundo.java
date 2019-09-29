443b716997f5faa219e6b00f171287bd322ca4b5
ADPLuis <luis.perez.nazco@gmail.com>
2019-09-29 17:48:32 +0100

    First commit

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String invertido = StringUtils.reverse("Hola Maven!");
+		System.out.println(invertido);
+	}
+
+}
